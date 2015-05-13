42969c4c8defeba06e9f6c0c6c802dcedd6c39ad
kaitlinmuth <kaitlin.muth@gmail.com>
2015-05-13 15:04:01 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var textField = "";
+
+$(document).ready(function () {
+    $('.keys').children("span").on('click', function(){
+        var keypress = $(this).text();
+        if (keypress == 'C'){
+            textField = "";
+        }
+        if (keypress == '='){
+            textField = parseInt(textField);
+        } else { textField = textField + keypress; }
+        $('.textField').text(textField);
+    });
+    $('.clear').on('click', function(){
+        textField = "";
+        $('.textField').text(textField);
+    })
+});
